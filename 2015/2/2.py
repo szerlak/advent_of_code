637a5b0fb3a98dad5c82f7c7f1868e26455b3506
Tomasz Szerle <szerletomasz@gmail.com>
2020-12-08 23:44:56 +0100

    2015 day 2

---
+++ 2015/2/2.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+from operator import mul
+
+data = [line.strip().split("x") for line in open("data.txt", "r")]
+for idx, line in enumerate(data):
+    data[idx] = sorted([int(i) for i in line])
+
+
+def paper(package):
+    sides = [mul(*side) for side in combinations(package, 2)]
+    return 2 * sum(sides) + min(sides)
+
+
+def ribbon(x, y, z):
+    return 2 * (x + y) + x * y * z
+
+
+print(sum([paper(package) for package in data]))
+print(sum([ribbon(*package) for package in data]))
